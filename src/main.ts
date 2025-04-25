25c873f93306bc7e44d37ecaa16d358ac7edb130
Frans <frans_421_12@hotmail.com>
2025-04-25 13:06:23 -0500

    chore: add app config

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,8 @@
 /// <reference types="@angular/localize" />
 
-import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
-import { provideHttpClient, withInterceptorsFromDi, HttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app/app.routes';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-    new TranslateHttpLoader(http, './assets/i18n/', '.json');
-
-bootstrapApplication(AppComponent, {
-    providers: [
-        provideRouter(routes),
-        provideHttpClient(withInterceptorsFromDi()),
-        provideTranslateService({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: httpLoaderFactory,
-                deps: [HttpClient],
-            },
-        }),
-        provideAnimations()
-    ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
