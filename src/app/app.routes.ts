54f0581053df8e78693d9c011b5aff6fdffbfb96
Frans <frans_421_12@hotmail.com>
2026-01-03 18:26:11 -0500

    refactor: home was moved to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { Archive } from './components/archive/archive';
-import { Home } from './components/home/home';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/es', pathMatch: 'full' },
