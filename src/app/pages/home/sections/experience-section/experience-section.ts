5350022783ccbe0d094ad1b444230699dc26c225
Frans <frans_421_12@hotmail.com>
2026-01-09 22:41:53 -0500

    feat: update experience section to use structured experience data

--- src/app/pages/home/sections/experience-section/experience-section.ts
+++ src/app/pages/home/sections/experience-section/experience-section.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import {
   NgbNav,
   NgbNavContent,
@@ -9,13 +10,30 @@ import {
   NgbNavOutlet,
 } from '@ng-bootstrap/ng-bootstrap';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ExperienceData } from 'src/app/services/experience-data/experience-data';
 
 @Component({
   selector: 'app-experience-section',
-  imports: [NgbNav, NgbNavItem, NgbNavItemRole, NgbNavLink, NgbNavLinkBase, NgbNavContent, NgbNavOutlet, TranslatePipe],
+  imports: [
+    NgbNav,
+    NgbNavItem,
+    NgbNavItemRole,
+    NgbNavLink,
+    NgbNavLinkBase,
+    NgbNavContent,
+    NgbNavOutlet,
+    DatePipe,
+    TranslatePipe,
+  ],
   templateUrl: './experience-section.html',
   styleUrl: './experience-section.scss',
 })
 export class ExperienceSection {
+  private experienceData = inject(ExperienceData);
+
   active = 0;
+  experiences = this.experienceData.getExperiences({
+    sortBy: 'endDate',
+    order: 'desc',
+  });
 }
