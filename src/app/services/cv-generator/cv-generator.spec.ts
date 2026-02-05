27e3d31a02a6a64fa2787036673c3b42b00d7f0d
Frans <frans_421_12@hotmail.com>
2026-02-04 21:31:03 -0500

    test: add provider translate service in cv generator and personal data services

--- src/app/services/cv-generator/cv-generator.spec.ts
+++ src/app/services/cv-generator/cv-generator.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { provideTranslateService } from '@ngx-translate/core';
 import { CvGenerator } from './cv-generator';
 
 describe('CvGenerator', () => {
   let service: CvGenerator;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideTranslateService({
+          defaultLanguage: 'es-PE',
+        }),
+      ],
+    });
     service = TestBed.inject(CvGenerator);
   });
 
