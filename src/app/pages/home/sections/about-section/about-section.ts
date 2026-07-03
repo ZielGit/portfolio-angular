00bda1f4a71644ce14e0d2d6ef8bd017717ae063
Frans <frans_421_12@hotmail.com>
2026-07-03 11:57:18 -0500

    feat: update about section to use dynamic data and improve translation handling

--- src/app/pages/home/sections/about-section/about-section.ts
+++ src/app/pages/home/sections/about-section/about-section.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { AnalyticsApi } from '../../../../services/analytics-api/analytics-api';
+import { PersonalData } from '../../../../services/personal-data/personal-data';
 
 @Component({
   selector: 'app-about-section',
@@ -10,4 +11,13 @@ import { AnalyticsApi } from '../../../../services/analytics-api/analytics-api';
 })
 export class AboutSection {
   analyticsApi = inject(AnalyticsApi);
+  private personalData = inject(PersonalData);
+
+  readonly paragraphKeys = computed(() => this.personalData.about().paragraphKeys);
+  readonly paragraphParams = computed(() => ({
+    years: this.personalData.yearsOfExperience(),
+    roles: this.personalData.formatRoles(),
+  }));
+  readonly profileImage = this.personalData.about().profileImage;
+  readonly profileImageAlt = this.personalData.about().profileImageAlt;
 }
