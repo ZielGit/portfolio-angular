2c44da60c0393997a10f6ae31efca501d683e44f
Frans <frans_421_12@hotmail.com>
2026-08-11 21:59:57 -0500

    feat: add Project model with visibility and relevant properties

---
+++ src/app/models/project-model.ts
@@ -0,0 +1,15 @@
+export type ProjectVisibility = 'public' | 'private';
+
+export interface Project {
+  readonly id: string;
+  readonly titleKey: string;
+  readonly descriptionKey: string;
+  readonly technologies: readonly string[];
+  readonly images: readonly string[];
+  readonly demoUrl?: string;
+  readonly githubUrl?: string;
+  readonly visibility: ProjectVisibility;
+  readonly featured?: boolean;
+  readonly startDate?: Date;
+  readonly endDate?: Date | null;
+}
